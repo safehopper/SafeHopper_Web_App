9a0b79a12940c11b3a68dc1e018227e039c1159e
Justin Terry <Justin.Terry@student.csulb.edu>
2019-11-20 12:14:50 -0800

    Some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import MapContainer from './components/MapContainer';
+import Header from './components/Header';
 
 
 function App() {
   return (
+    <div>
+      <Header />
       <MapContainer />
+    </div>
   );
 }
 
